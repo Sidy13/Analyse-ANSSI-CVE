09e819288c7b8eda09a8a0075226faee812d539d
Sidy13 <sidydcr.pro@gmail.com>
2025-06-14 16:05:19 +0200

    Étape 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello GitHub!')
+import feedparser
+url = "https://www.cert.ssi.gouv.fr/avis/feed"
+rss_feed = feedparser.parse(url)
+entrée = 0
+for entry in rss_feed.entries:
+    print("Entrée numéro :", entrée)
+    print("\nTitre :", entry.title)
+    print("\nDescription:", entry.description)
+    print("\nLien :", entry.link)
+    print("\nDate :", entry.published) 
+    entrée+=1
+    print("\n\n")       
